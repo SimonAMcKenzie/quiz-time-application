e1292857505a347f564cd9fda27e9149b8473823
Simon_McKenzie <simon-mckenzie@hotmail.co.uk>
2023-11-04 13:41:25 +0000

    added new repo files and some pseudo code

---
+++ assets/js/questions.js
@@ -0,0 +1,6 @@
+//create variable and fgunction for get and set for start button
+//create variable called questions 
+//within question object create an array to list all all questions and answers 
+//then create an if statement that defines the right answer and the wrong answers, and for each the response that appears at the bottom of all the answers as "Right" or "Wrong" 
+//
+
