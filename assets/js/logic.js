e1bf6a7e2037d14cb0cd2a3e713be4ff2d22e003
Simon Mckenzie <145777422+SimonAMcKenzie@users.noreply.github.com>
2023-11-04 15:25:18 +0000

    Update logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+//define variables for all required DOM nodes
+//var startScreenEl = document.getElementById ('#start-screen')
+// var btnStart = document.getElementId('#start')
+// var questionsEl = document.getElementById('#questions')
+
+
+// if statement for questions
+// if questions [array number] .answers === questions[array number].correct === 2; set #start screen: ""
+// else --10 from timer variables
+
+//create function for button within "questions"
+//create button div element in questions div
+//use for loop to 
+
+
