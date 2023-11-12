31fc0413a79b4123de5c04ea485117cf109442d3
SimonAMcKenzie <simon-mckenzie@hotmail.co.uk>
2023-11-12 19:59:52 +0000

    removed unnecessary text from JavaScript

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -27,12 +27,4 @@ var feedbackEl = document.getElementById('feedback')
 // the user can then see where there score ranks by going clicking the view scores text on the top left
 
 
-WHEN I click the start button a timer starts and I am presented with a question
 
-WHEN I answer a question I am presented with another question
-
-WHEN I answer a question incorrectly time is subtracted from the clock
-
-WHEN all questions are answered or the timer reaches 0 the game is over
-
-WHEN the game is over I can save my initials and score
