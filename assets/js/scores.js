101d64468edeb10b5b16a4ce3d679ead45bf27b5
SimonAMcKenzie <simon-mckenzie@hotmail.co.uk>
2023-11-13 10:56:06 +0000

    Moved scoring + timing functionality to scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,24 @@
-var correctAnswerScore = 
-var incorrectAnswer
-var clearHighScoresEl = document.getElementById ('#start-screen')
+// Variables for scores 
+var correctAnswerScore = 0;
+var incorrectAnswerScore = 0;
+var highScores = []; // This array stores high score data
+
+// This is the function to update scroes for correct answers
+function updateCorrectAnswerScore() {
+    correctAnswerScore += 100; // Adjust this number to increase score value. The inverse of this is -=
+}
+
+// This is the functionality needed to deduct time from the clock when incoorect answers are selected
+function updateIncorrectAnswerScore() {
+    if (timeLeft > 10) {
+        timeLeft -= 10;
+    } else {
+        timeLeft = 0;
+    }
+    //incorrectAnswerScore -= 5; can be added if I deciding that wrong answers should also deduct points from score 
+}
+
+// This is the function needed to make the clear high score function work, if user does not want score recorded
+function clearHighScores() {
+    highScores = []; 
+}
